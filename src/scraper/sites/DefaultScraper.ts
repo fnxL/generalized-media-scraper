f548fa075cded1b848dd9f9cfdda8be4ee49efec
Ajay Kanki <48607132+fnxL@users.noreply.github.com>
2024-06-24 01:18:57 +0530

    refactor: add overloads for the scrape method

--- src/scraper/sites/DefaultScraper.ts
+++ src/scraper/sites/DefaultScraper.ts
@@ -7,8 +7,24 @@ class DefaultScraper extends BaseScraper {
         super(opts);
     }
 
-    async scrape(url: string, selector: Selectors) {
-        return this._scrape(url, selector);
+    async scrape(
+        url: string,
+        folder: string,
+        selector: Selectors,
+    ): Promise<any>;
+    async scrape(url: string, selector: Selectors): Promise<any>;
+    async scrape(url: string, arg2: string | Selectors, arg3?: Selectors) {
+        let folder: string | undefined;
+        let selector: Selectors;
+
+        if (typeof arg2 === 'string') {
+            folder = arg2;
+            selector = arg3 as Selectors;
+        } else {
+            selector = arg2;
+        }
+
+        return this._scrape(url, selector, folder);
     }
 }
 
